5cff25a577ede64b45bdb4701045d5d0b96ac8da
Aachal Gupta <aachalgupta0411@gmail.com>
2025-05-17 17:22:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-let count = 0;
-function increment(){
-    count++;
-    document.getElementById("CV").innerHTML=`Counter Value : ${count}`
-
+function show() {
+    document.getElementById('mytext').style.display="block";
 }
-function decrement(){
-    count--;
-    document.getElementById("CV").innerHTML=`Counter Value : ${count}`
 
-
-}
-function reset(){
-    count = 0;
-    document.getElementById("CV").innerHTML=`Counter Value : ${count}`
-
-
+function hide() {
+document.getElementById('mytext').style.display="none";
 }
